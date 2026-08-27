e62163f55fc9a5a5249a1442d6fbbaf3021f0b36
MartinOtero <martinoteroavila@gmail.com>
2026-08-27 21:28:44 +0200

    Creo que por fin fun capaz de solucionar o tema das ligazons entre paxinas

--- scripts/xeral.js
+++ scripts/xeral.js
@@ -1,30 +1,43 @@
 
 
+//solucion para problema de rutas relativas en localhost y en servidor
+let RUTA_BASE = "";
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+    RUTA_BASE = "/"; 
+} else {
+    RUTA_BASE = "/paxina_fervenza/"; 
+}
+
+
 //Neste .js poñemos codigos que usan todas as páxinas da web. Noutros casos é mellor diferenciar
 // para qe as paxinas non carguen código que non usan.  
 
 
 // Pezas comuns en varias páxinas. Importar dende o script e ligar co ID
-    fetch('paxina_fervenza/pezas/nav.html')
+    fetch(RUTA_BASE+`pezas/nav.html`)
         .then(respuesta => respuesta.text())
-        .then(codigoHtml => {
-            // Cuando lo tenga, lo mete dentro de nuestro hueco
-            document.getElementById('contenedor-menu').innerHTML = codigoHtml;
-        });
+        .then(datos => {
+        // La traducción: cambiamos el comodín por la ruta real
+        let htmlCorregido = datos.replaceAll("{RAIZ}", RUTA_BASE);
+        document.getElementById("contenedor-menu").innerHTML = htmlCorregido;
+    });
 
-    fetch('paxina_fervenza/pezas/pe.html')
+    fetch(RUTA_BASE+'pezas/pe.html')
         .then(respuesta => respuesta.text())
-        .then(codigoHtml => {
-            // Cuando lo tenga, lo mete dentro de nuestro hueco
-            document.getElementById('contenedor-footer').innerHTML = codigoHtml;
-        });
+        .then(datos => {
+        // La traducción: cambiamos el comodín por la ruta real
+        let htmlCorregido = datos.replaceAll("{RAIZ}", RUTA_BASE);
+        document.getElementById("contenedor-footer").innerHTML = htmlCorregido;
+    });
 
-    fetch('paxina_fervenza/pezas/lateral.html')
+    fetch(RUTA_BASE+'pezas/lateral.html')
         .then(respuesta => respuesta.text())
-        .then(codigoHtml => {
-            // Cuando lo tenga, lo mete dentro de nuestro hueco
-            document.getElementById('barra-lateral').innerHTML = codigoHtml;
-        });
+        .then(datos => {
+        // La traducción: cambiamos el comodín por la ruta real
+        let htmlCorregido = datos.replaceAll("{RAIZ}", RUTA_BASE);
+        document.getElementById("barra-lateral").innerHTML = htmlCorregido;
+    });
+        
 
 
 
